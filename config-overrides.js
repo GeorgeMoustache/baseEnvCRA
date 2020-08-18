191c0a0d70fb8fa5a7a5b9fb24d787e5abd69256
GeorgeMoustache <ap062162@hotmail.com>
2020-08-18 11:56:27 +0800

    init commit

--- config-overrides.js
+++ config-overrides.js
@@ -5,6 +5,7 @@ const path = require('path');
 /* config-overrides.js */
 module.exports = function override(config, env) {
 	config = rewireAliases.aliasesOptions({
+    '@': path.resolve(__dirname, `${paths.appSrc}/`),
     '@router': path.resolve(__dirname, `${paths.appSrc}/router/`),
     '@components': path.resolve(__dirname, `${paths.appSrc}/components/`),
     '@assets': path.resolve(__dirname, `${paths.appSrc}/assets/`),
